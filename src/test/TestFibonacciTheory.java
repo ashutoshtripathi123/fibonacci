de292620552d9588cea2592c77ed14d2bba4104f
ashutoshtripathi123 <ashutoshtripathi123@gmail.com>
2015-12-09 16:09:26 +0530

    changed from tabs to spaces

--- src/test/TestFibonacciTheory.java
+++ src/test/TestFibonacciTheory.java
@@ -10,21 +10,21 @@ import org.junit.runner.RunWith;
 public class TestFibonacciTheory
 {
 
-	@DataPoints
-	public static int[] VALUES =
-	{ 0, 1, 2, 3, 45 };
+    @DataPoints
+    public static int[] VALUES =
+    { 0, 1, 2, 3, 45 };
 
-	@Theory
-	public void seeds(int n)
-	{
-		assumeTrue(n <= 1); // ignores values > 1
-		assertEquals(n, Fibonacci.calculate(n));
-	}
+    @Theory
+    public void seeds(int n)
+    {
+        assumeTrue(n <= 1); // ignores values > 1
+        assertEquals(n, Fibonacci.calculate(n));
+    }
 
-	@Theory
-	public void recurrence(int n)
-	{
-		assumeTrue(n > 1); // ignores values <= 1
-		assertEquals(Fibonacci.calculate(n - 1) + Fibonacci.calculate(n - 2), Fibonacci.calculate(n));
-	}
+    @Theory
+    public void recurrence(int n)
+    {
+        assumeTrue(n > 1); // ignores values <= 1
+        assertEquals(Fibonacci.calculate(n - 1) + Fibonacci.calculate(n - 2), Fibonacci.calculate(n));
+    }
 }
